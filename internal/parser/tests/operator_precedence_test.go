902ae0bc1b7dbce8a5796360a21c16c2bb28cfe2
Denis Kozarezov <kozarezov.denis@wb.ru>
2025-06-09 18:52:30 +0300

    added tests for operator precedence

---
+++ internal/parser/tests/operator_precedence_test.go
@@ -0,0 +1,84 @@
+package tests
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"interpreter/internal/lexer"
+	"interpreter/internal/parser"
+)
+
+func TestOperatorPrecedence(t *testing.T) {
+	for _, tt := range []struct {
+		source   string
+		expected string
+	}{
+		{
+			"-a * b",
+			"((-a) * b)",
+		},
+		{
+			"!-a",
+			"(!(-a))",
+		},
+		{
+			"a + b + c",
+			"((a + b) + c)",
+		},
+		{
+			"a + b - c",
+			"((a + b) - c)",
+		},
+		{
+			"a * b * c",
+			"((a * b) * c)",
+		},
+		{
+			"a * b / c",
+			"((a * b) / c)",
+		},
+		{
+			"a + b / c",
+			"(a + (b / c))",
+		},
+		{
+			"a + b * c + d / e - f",
+			"(((a + (b * c)) + (d / e)) - f)",
+		},
+		{
+			"3 + 4; -5 * 5",
+			"(3 + 4)((-5) * 5)",
+		},
+		{
+			"5 > 4 == 3 < 4",
+			"((5 > 4) == (3 < 4))",
+		},
+		{
+			"5 < 4 != 3 > 4",
+			"((5 < 4) != (3 > 4))",
+		},
+		{
+			"3 + 4 * 5 == 3 * 1 + 4 * 5",
+			"((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
+		},
+		{
+			"3 + 4 * 5 == 3 * 1 + 4 * 5",
+			"((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
+		},
+	} {
+		t.Run(tt.source, func(t *testing.T) {
+			// 1. Arrange
+			l := lexer.NewLexer(strings.NewReader(tt.source))
+			p := parser.NewParser(l)
+
+			// 2. Act
+			program := p.Parse()
+			got := program.String()
+
+			// 3. Assert
+			require.Len(t, p.Errors(), 0)
+			require.Equal(t, tt.expected, got)
+		})
+	}
+}
