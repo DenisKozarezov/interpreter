4b0798c81905bf33621aaf87d8276b3ee64b8e52
kozarezov.denis <kozarezov.denis@rwb.ru>
2026-07-04 19:53:51 +0300

    Added logging functions in built-in, added an example

--- internal/evaluator/builtin.go
+++ internal/evaluator/builtin.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "interpreter/internal/object"
+import (
+	"interpreter/internal/object"
+
+	"log"
+)
 
 var builtins = map[string]*object.BuiltIn{
 	"len": {
@@ -18,6 +22,56 @@ var builtins = map[string]*object.BuiltIn{
 				return &object.Integer{Value: int64(len(arg.Items))}
 			default:
 				return newRuntimeError("argument to 'len' not supported, got %s", arg.Type())
+			}
+		},
+	},
+
+	"log": {
+		Function: func(args ...object.Object) object.Object {
+			if len(args) == 0 {
+				return newRuntimeError("no arguments were passed")
+			}
+
+			logArgs := make([]any, len(args))
+			for i := range args {
+				logArgs[i] = args[i].Inspect()
+			}
+
+			log.Println(logArgs...)
+			return object.NULL
+		},
+	},
+
+	"logf": {
+		Function: func(args ...object.Object) object.Object {
+			argsLen := len(args)
+
+			switch argsLen {
+			case 0:
+				return newRuntimeError("no arguments were passed")
+			case 1:
+				log.Println(args[0].Inspect())
+				return object.NULL
+			default:
+				format := args[0]
+				if format.Type() != object.STRING_TYPE {
+					return newRuntimeError("the first argument is invalid, expected STRING, but got = %s", format.Type())
+				}
+
+				logArgs := make([]any, 0, argsLen-1)
+				for i := 1; i < argsLen; i++ {
+					switch args[i].Type() {
+					case object.INTEGER_TYPE:
+						logArgs = append(logArgs, args[i].(*object.Integer).Value)
+					case object.BOOLEAN_TYPE:
+						logArgs = append(logArgs, args[i].(*object.Boolean).Value)
+					default:
+						logArgs = append(logArgs, args[i].Inspect())
+					}
+				}
+
+				log.Printf(format.Inspect(), logArgs...)
+				return object.NULL
 			}
 		},
 	},
